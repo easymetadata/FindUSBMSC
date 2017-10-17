68835202e1b7e279d7362221ae68b5326facbf01
EasyMetaData <ddym@redrocktx.com>
2017-10-16 22:03:51 -0500

    Update FindUSBMSC.py
    v20171016 - Logic cleanup. Improve pid and vid parsing. Added list of unique devices. Added options parser

--- FindUSBMSC.py
+++ FindUSBMSC.py
@@ -5,25 +5,79 @@
 #		Refs: https://www.mac4n6.com/blog/2016/11/13/new-macos-sierra-1012-forensic-artifacts-introducing-unified-logging
 #		-
 # Author: David Dym
-# Version 20170720 - Ported from perl to python. Derived from Jason Hale's usbmsc.pl script version 20130125
+# v20170720 - Ported from perl to python. Derived from Jason Hale's usbmsc.pl script version 20130125
+# v20171003 - changes to regex pattern matching to account for commma change in system log. Logic changes to pid matching. Fixes to logic. 
+# v20171016 - Logic cleanup. Improve pid and vid parsing. Added list of unique devices. Added options parser
 
 import os
+import time
+from datetime import datetime, timedelta
 import sys, csv, sqlite3
-import os,fnmatch
-import datetime
+import fnmatch
 import re
 import urllib2
 import gzip
+import numpy
+from optparse import OptionParser
 
+version = 'v20171016'
 url = "http://www.linux-usb.org/usb.ids"
-repattern = "(^[a-z]{3}\s{1,2}\d{1,2})\s(\d\d:\d\d:\d\d)\s(.+)\s[a-z]+\[\d+\]:\sUSBMSC\s.+:\s(.*)0x(.*)\s0x(.*)\s0x(.*)"
-dirpath = sys.argv[1]
+
+tempDFileName = ""
+USBMSCmatches = []
+USBMSCdevices = []
+
+print '\n========================================================================'
+print 'FindUSBMSC ', version, ' -- by David Dym'
+print '========================================================================\n'
+
+def GetOptions():
+    '''Get needed options for processesing'''
+    usage = "usage: %prog -d LOGPATH";
+    options = OptionParser(usage=usage);
+
+    options.add_option("-d",
+                   action="store",
+                   type="string",
+                   dest="logpath",
+                   default=True, 
+                   help="Path to the system logs");
+    return options;
+
+def ParseOptions():
+        # Get options
+        options = GetOptions()
+        (opts,args) = options.parse_args()
+
+        # The meta will store all information about the arguments passed #
+        meta = {
+            'logpath':opts.logpath
+            }
+        # Test arguments passed #
+        if os.path.exists(meta['logpath']):
+            pass
+        else:
+            options.error("Unable to proceed. Check the proper command syntax using -h\n")
+        # Return meta to caller #
+        return meta;
+
+
+# deletes file if older than 2 days
+def oldFileCleanup(file, days):
+	current_time = time.time()
+	creation_time = os.path.getctime(file)
+	if (current_time - creation_time) // (24 * 3600) >= days:
+		os.unlink(file)
 
 def get_usbid_file(url):
 	#url="http://.../"
 	# Translate url into a filename
 	filename = url.split('/')[-1]
 
+	#check age of usb.ids and delete if older than 2 days
+	if os.path.exists(filename):
+		oldFileCleanup(filename, 2)
+		
 	if not os.path.exists(filename):
 	  outfile = open(filename, "w")
 	  outfile.write(urllib2.urlopen(url).read())
@@ -44,24 +98,6 @@ def find_files(directory, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
 
-def analyze_file(filename):	
-	#print 'File:', filename
-	
-	#check for gzip File
-	if ".gz" in filename:
-		filename = compressedLog(filename)
-
-	bufsize = 65536
-	with open(filename) as infile: 
-		while True:
-			lines = infile.readlines(bufsize)
-			if not lines:
-				break
-			for line in lines:
-				usbDev = findUSBMSC(line)
-				if usbDev:
-					print usbDev
-
 def compressedLog(filename):
 	with gzip.open(filename, 'rb') as infile:
 		tempDFileName = "tmp000.log"
@@ -70,33 +106,89 @@ def compressedLog(filename):
 				outfile.write(line)
 	return filename
 
-def findUSBMSC(line):
-	matches = re.match(r"(^[a-z]{3}\s{1,2}\d{1,2})\s(\d\d:\d\d:\d\d)\s(.+)\s[a-z]+\[\d+\]:\sUSBMSC\s.+:\s(.*)0x(.*)\s0x(.*)\s0x(.*)", line, re.IGNORECASE)
-	
-	if matches:
-		logDate = matches.group(1) + " "  + matches.group(2)
-		serial = matches.group(4)
-		vid = matches.group(5)
-		pid = matches.group(6)
+def USBMSCEntry(logDate, host, serial, vid, pid, miscother):
+	return "%s, %s, %s, %s, %s, %s" % (logDate, host, serial, vid, pid, miscother.strip())
 
-		#if pid:
-		usbs = get_usbid_file(url)
-		for line2 in usbs:
-			#print "\n test: %s" % line2
-			if re.match(r"^\t", line2):
-				if pid in line2:
-					if re.match("\t0*" + pid + "\s",line2):
-						return "%s, %s, %s, %s, %s" % (logDate,serial, vid, pid, line2.strip())
+def findUSBMSC(filename):
+	with open(filename) as infile: 
+		while True:
+			lines = infile.readlines()
+			if not lines:
+				break
+			for line in lines:
 
-#header
-print "date, serial, vid, pid, usb info"
+				matches = re.match(r"(^[a-z]{3}\s{1,2}\d{1,2})\s(\d\d:\d\d:\d\d)\s(.+)\s[a-z]+\[\d+\]:\sUSBMSC\s.+:\s(.*)\s0x(.*)\s0x(.*)\s0x(.*)", line, re.IGNORECASE)		
+		
+				if matches:
+					#print line
+					logDate = matches.group(1) + " "  + matches.group(2)
+					host = matches.group(3).strip()
+					serial = matches.group(4).strip()
+					vid = matches.group(5).strip()
+					pid = matches.group(6).strip()
+					miscother = matches.group(7)
 
-#read each file		
+					USBmatch = USBMSCEntry(logDate, host, serial, vid, pid, miscother)
+					if USBmatch not in USBMSCmatches:
+						USBMSCmatches.append(USBmatch)
+
+def matchUSBids(line, usbs):
+	manufacturer = ""
+	items = line.split(',')
+	vid = items[3].strip()
+	pid = items[4].strip()
+
+	for line2 in usbs:		
+		
+		if line2.startswith("0"):
+			if vid in line2:
+				manufacturer = "%s" % re.sub(r"^0" + vid, '', line2).strip()
+		elif line2.startswith("\t"):
+			if pid in line2:
+				if manufacturer:
+					line += " [%s" % manufacturer
+					line += ", %s" % line2.replace(pid,'').strip()
+					line += "]"
+					manufacturer = ""
+	if manufacturer:
+		line += " [%s]" % manufacturer
+	USBMSCdevices.append(line)
+	return line
+
+#### Main ####
+meta = ParseOptions()
+dirpath = meta['logpath']
+
+# get usbid's
+usbidlist = get_usbid_file(url)
+
+print "Logs processed"
+#read each file
 for filename in find_files(dirpath, '*.log*'):
-	#print 'File:', filename
+	print filename
+	
+	if "system" in filename:
+		#check for gzip File
+		if ".gz" in filename:
+			filename = compressedLog(filename)
+		
+		#process system log
+		findUSBMSC(filename)
 
-	#analyze each file
-	analyze_file(filename)
+print "\nUSBMSC devices"
+for line in USBMSCmatches:
+	print matchUSBids(line, usbidlist)
 
+#lets get distinct devices
+print "\nUnique devices"
+uDevices = []
+for item in USBMSCdevices:
+	items = item.split(',')[2:]
+	if ", ".join(items) not in uDevices:
+		uDevices.append(", ".join(items))
+for item in uDevices:
+	print item
 
-# usbids into array
+#cleanup temp file
+if tempDFileName:
+	os.remove(tempDFileName)
